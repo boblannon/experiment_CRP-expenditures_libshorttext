6be7df3e47ce4eeded0491719ae7388b8add8cc2
Bob Lannon <blannon@gmail.com>
2013-12-12 17:21:01 -0500

    updated train/test script

--- scripts/train_and_test.py
+++ scripts/train_and_test.py
@@ -1,8 +1,3 @@
-# -*- coding: utf-8 -*-
-# <nbformat>3.0</nbformat>
-
-# <codecell>
-
 import sys
 import os
 import csv
@@ -14,24 +9,20 @@ import pandas as pd
 from libshorttext.converter import Text2svmConverter, FeatureGenerator, TextPreprocessor, convert_text
 from libshorttext.classifier import train_converted_text
 
-# <codecell>
+import local_settings as ls
 
-project_name = 'crp_expenditures_nov4_7-16'
+project_name = ls.project_name
+BASE_DIR = ls.BASE_DIR
 
-# <codecell>
-
-!tree
-
-# <codecell>
-
-BASE_DIR = '/home/blannon/Dropbox/dev/experiments/CRP-expenditures_libshorttext/'
-
-CONVERTER_DIR = os.path.join(BASE_DIR, 'converters')
 
 DATA_DIR = os.path.join(BASE_DIR, 'data')
 CLEAN_DIR = os.path.join(DATA_DIR, 'clean')
 TRAIN_DIR = os.path.join(DATA_DIR, 'labeled', 'train')
 TEST_DIR = os.path.join(DATA_DIR, 'labeled', 'test')
+CONVERTER_DIR = os.path.join(BASE_DIR, 'converters')
+MODEL_DIR = os.path.join(BASE_DIR, 'models')
+RESULT_DIR = os.path.join(BASE_DIR, 'results')
+SETTINGS_DIR = os.path.join(BASE_DIR, 'settings')
 
 train_csv = os.path.join(CLEAN_DIR, project_name + '_train.csv')
 test_csv = os.path.join(CLEAN_DIR, project_name + '_test.csv')
@@ -39,82 +30,74 @@ test_csv = os.path.join(CLEAN_DIR, project_name + '_test.csv')
 train_svm = os.path.join(TRAIN_DIR, project_name + '_train.svm')
 test_svm = os.path.join(TEST_DIR, project_name + '_test.svm')
 
-MODELS_DIR = os.path.join(BASE_DIR, 'models')
+settings_json = os.path.join(SETTINGS_DIR, project_name + '_settings.json')
+        
+converter_path = os.path.join(CONVERTER_DIR, project_name + '.converter')
 
-# <codecell>
+model_path = os.path.join(MODEL_DIR, project_name+'.model')
 
-settings = {}
+result_path = os.path.join(RESULT_DIR, project_name+'.result')
 
-# STEMMING: 1 to use porter, 0 for no stemming, or give your 
-#           own stemmer that maps from a list of tokens to a 
-#           list of stemmed tokens
-settings['stemming'] = '1'
+def make_dirs(dirlist):
+    for d in dirlist:
+        if not os.path.exists(d):
+            os.makedirs(d)
 
-# STOPWORDS: 1 to use default stops, 0 for no stops, or give
-#            a list of stopwords to be used
-settings['stopword'] = '0'
+def make_options():
+    options = {}
 
-# NGRAM: 1 to use bigram, 0 to use unigram
-settings['ngram'] = '0'
+    # STEMMING: 1 to use porter, 0 for no stemming, or give your 
+    #           own stemmer that maps from a list of tokens to a 
+    #           list of stemmed tokens
+    options['stemming'] = ls.stemming
 
-json.dump(settings, 
-          open( 
-               os.path.join(
-                            'settings',
-                            project_name + '_settings.json'), 
-               'w'),
-          indent=2)
+    # STOPWORDS: 1 to use default stops, 0 for no stops, or give
+    #            a list of stopwords to be used
+    options['stopword'] = ls.stopword
 
-# <codecell>
+    # NGRAM: 1 to use bigram, 0 to use unigram
+    options['ngram'] = ls.ngram
 
-!cat settings/crp_expenditures_nov4_7-6_settings.json
+    json.dump(options, open(options_json, 'w'), indent=2)
+    return options
 
-# <codecell>
+make_dirs([DATA_DIR, CLEAN_DIR, TRAIN_DIR, TEST_DIR, MODEL_DIR, RESULT_DIR, 
+            SETTINGS_DIR, CONVERTER_DIR])
 
-tp_option_str = "-stopword {stopword} -stemming {stemming}".format(**settings)
-text_processor = TextPreprocessor(option=tp_option_str)
+options = make_options()
 
-# <codecell>
+if not os.path.exists(converter_path):
+    # Make text processor
+    tp_option_str = "-stopword {stopword} -stemming {stemming}".format(**options)
+    text_processor = TextPreprocessor(option=tp_option_str)
 
-fg_option_str = "-feature {ngram}".format(**settings)
-feature_generator = FeatureGenerator(option=fg_option_str)
+    # Make feature generator
+    fg_option_str = "-feature {ngram}".format(**options)
+    feature_generator = FeatureGenerator(option=fg_option_str)
 
-# <codecell>
+    # Make converter
+    text_converter = Text2svmConverter()
+    text_converter.feat_gen = feature_generator
+    text_converter.text_prep = text_processor
 
-text_converter = Text2svmConverter()
+    # Convert Text
+    convert_text(train_csv, text_converter, train_svm)
 
-# <codecell>
+    # Save Converter
+    text_converter.save(converter_path)
+else:
+    text_converter = Text2svmConverter()
+    text_converter.load(converter_path)
 
-text_converter.feat_gen = feature_generator
-text_converter.text_prep = text_processor
+if not os.path.exists(model_path):
+    text_model = train_converted_text(train_svm, text_converter)
+    text_model.save(model_path)
+else:
+    text_model = TextModel(model_path)
 
-# <codecell>
+prediction_result = predict_text(test_csv, text_model)
+prediction_result.save(result_path, analyzable=True)
 
-
-# <codecell>
-
-convert_text(train_csv, text_converter, train_svm)
-
-# <codecell>
-
-text_converter.save(os.path.join(CONVERTER_DIR, project_name + '.converter'))
-
-# <codecell>
-
-!head data/labeled/train/crp_expenditures_nov4_7-16_train.svm
-
-# <codecell>
-
-!head data/clean/crp_expenditures_nov4_7-16_train.csv
-
-# <codecell>
-
-text_model = train_converted_text(train_svm, text_converter)
-
-# <codecell>
-
-text_model.save(os.path.join(MODEL_DIR,project_name+'.model'))
-
-# <codecell>
-
-
+sys.stderr.write('prediction accuracy was {}\n'.format(
+                  prediction_result.get_accuracy()))
+sys.stderr.write('prediction results analyzable in {}\n'.format(result_path))
